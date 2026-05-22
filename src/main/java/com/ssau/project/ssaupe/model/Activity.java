5584284540d7fc47868651125eb8fb72b141e526
bugde <bugdeer11@gmail.com>
2026-05-23 00:41:47 +0400

    after merge hollow commit

--- src/main/java/com/ssau/project/ssaupe/model/Activity.java
+++ src/main/java/com/ssau/project/ssaupe/model/Activity.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime; // ВАЖНО: Используем LocalDateTime
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Setter
